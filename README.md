386083f85b49c7e971822edeb9b3b46a89a228b8
Anshul Raj Verma <arv.anshul.1864@gmail.com>
2025-02-26 12:07:04 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Search Emoji: Kunkun Extension
+
+A Kunkun extension to search and copy emoji in clipboard faster than any native method.
+
+![screenshot](public/screenshot.png)
+
+> \[!WARNING\]
+>
+> Currently in development phase.
---
+++ src/index.ts
@@ -0,0 +1,105 @@
+import {
+  Action,
+  clipboard,
+  expose,
+  Icon,
+  IconEnum,
+  List,
+  Markdown,
+  open,
+  TemplateUiCommand,
+  toast,
+  ui,
+} from "@kksh/api/ui/template";
+import emojiGroup from "unicode-emoji-json/data-by-group.json";
+
+function getEmojisSections(): List.Section[] {
+  let sections = [];
+  for (const category of emojiGroup) {
+    let items = [];
+    for (const emoji of category.emojis) {
+      items.push(
+        new List.Item({
+          title: emoji.name,
+          value: emoji.emoji,
+          icon: new Icon({
+            type: IconEnum.Text,
+            value: emoji.emoji,
+            bgColor: "#000",
+          }),
+        })
+      );
+    }
+    sections.push(
+      new List.Section({
+        title: category.name,
+        items: items,
+      })
+    );
+  }
+  return sections;
+}
+
+class Emoji extends TemplateUiCommand {
+  async load(): Promise<void> {
+    // UI tweaks
+    ui.setSearchBarPlaceholder("Search Emoji Name...");
+
+    let emojiSections = getEmojisSections();
+    return ui.render(
+      new List.List({
+        sections: emojiSections,
+        detail: new List.ItemDetail({
+          children: [
+            new Markdown("# Emoji Extension"),
+            new Markdown(`## Selected Emoji: ...`),
+            new Markdown(`Yet to implement...`),
+            new Markdown(
+              "### Author is [@arv-anshul](https://github.com/arv-anshul)"
+            ),
+          ],
+          width: 50,
+        }),
+        actions: new Action.ActionPanel({
+          title: "Emoji Action",
+          items: [
+            new Action.Action({
+              title: "Open Extenstion Repo",
+              value: "open-ext-repo",
+              icon: new Icon({
+                type: IconEnum.Iconify,
+                value: "simple-icons:github",
+              }),
+            }),
+          ],
+        }),
+      })
+    );
+  }
+
+  onActionSelected(value: string): Promise<void> {
+    switch (value) {
+      // case "copy-slug":
+      //     return clipboard.writeText()
+      case "open-ext-repo":
+        return open.url("https://github.com/arv-anshul/kunkun-search-emoji");
+      default:
+        toast.error("Action Fallback!");
+        return Promise.resolve();
+    }
+  }
+
+  async onListItemSelected(value: string): Promise<void> {
+    return clipboard.writeText(value).then(
+      () => {
+        toast.success(`Copied: ${value}`);
+      },
+      (e) => {
+        console.error(e);
+        toast.error(`Error while copying emoji ${value}!`);
+      }
+    );
+  }
+}
+
+expose(new Emoji());
